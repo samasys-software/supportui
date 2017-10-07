60ec014879c45f1ed8e0b55476af1ba79a81ee7b
kathirvelnatarajan <30907665+kathirvelnatarajan@users.noreply.github.com>
2017-10-07 02:23:19 -0400

    final report

--- modules/report/controller.js
+++ modules/report/controller.js
@@ -18,26 +18,59 @@ angular.module('Report')
         return phoneNumber+isdCode;
     }
 })
+.filter("customDate",function(){
+    return 
+})
 .controller('ReportController',
            ['$scope','$rootScope','$location','AuthenticationService',
            function($scope,$rootScope,$location,AuthenticationService){
                var employeeId=$rootScope.globals.currentUser.employee.employeeId;
-               
+               console.log($scope.start_Date);
+                   console.log($scope.end_Date);
               $scope.getSelectedType=function(){
                $scope.type=document.getElementById("selectedType").value;
+                  AuthenticationService.supportReport('null',$scope.startDate,$scope.endDate,document.getElementById("selectedType").value,'ALL',function(data){
+                      $scope.myReports=data.serviceSupport;
+                       });
                console.log($scope.type);
                   };  
-             
-              
+               $scope.issue= {};
+            
                $scope.getReport=function(){
-               
-               AuthenticationService.supportReport('null',$scope.startDate,$scope.endDate,document.getElementById("selectedType").value,'ALL',function(data){
+                   console.log($scope.issue.startdate);
+                   console.log($scope.issue.enddate);
+                   $scope.checkErr = function(startDate,endDate){
+    $scope.errMessage = '';
+    $scope.curDate = new Date();
+
+    if(startDate < endDate){
+      $scope.errMessage = 'End Date should be greate than start date';
+      return false;
+    }
+    if(startDate < curDate){
+       $scope.errMessage = 'Start date should not be before today.';
+       return false;
+    }
+
+  };
+                   AuthenticationService.supportReport('null',$scope.issue.startdate,$scope.issue.enddate,document.getElementById("selectedType").value,'ALL',function(data){
                    $scope.myReports=data.serviceSupport;
                    
                    console.log(data);
                    
                });
-                   };
+               }                
+               
+    
+             
+               
+             AuthenticationService.supportReport('null',$scope.startDate,$scope.endDate,document.getElementById("selectedType").value,'ALL',function(data){
+                   $scope.myReports=data.serviceSupport;
+                   
+                   console.log(data);
+                   
+               });
+                   
                
                
            }]);
